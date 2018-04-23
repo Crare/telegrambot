5de8712af19aca72d5f8e22498c75e00f9206a10
crare <crazunen@gmail.com>
2018-04-23 22:07:19 +0300

    fixing ruuvi

--- data_parsers/ruuvi.js
+++ data_parsers/ruuvi.js
@@ -17,7 +17,7 @@ exports.getRuuviTagData = (tag, callBack) => {
 	  	} else {
 	  		// results is an array consisting of messages collected during execution
 	  		console.log('results: %j', results);
-	  		let ruuvitag = JSON.parse(results[0].replace("'", '"'));
+	  		let ruuvitag = JSON.parse(results[0].replace(/'/g, '"'));
 	  		ruuvitag.name = tag.name;
 	  		let output = "";
 	  		if(results[0].length > 0) {
