8ff93608753793a4847875192b5a9c34360fa0fd
crare <Crare@users.noreply.github.com>
2019-09-16 20:40:48 +0300

    adding unit tests

--- test/test.js
+++ test/test.js
@@ -1,31 +1,120 @@
 const assert = require('assert');
-const settings = require('./settings.json');
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+const settings = require('../settings.json');
+
+const sunrise_sunset = require('../data_parsers/sunrise-sunset.js');
+const train_parser = require('../data_parsers/vr-trains.js');
+const weather_parser = require('../data_parsers/weather.js');
+const news_parser = require('../data_parsers/news3.js');
+
+const test_data = {
+  sunrise: {
+    place: {
+      nimi: settings.sun_place_name,
+      countrycode: settings.countrycode,
+      lat: settings.sun_at_lat,
+      lng: settings.sun_at_lon
+    }
+  },
+  train: {
+    stop1: "Helsinki",
+    stop2: "Tamere",
+    amount: 10,
+  },
+  weather: {
+    place: {
+      name: "Helsinki",
+      countrycode: "FI"
+    },
+    days: 5
+  }
+};
+
+const debug = false;
+const YELLOW ='\033[1;33m';
+const NEUTRAL = '\033[0m';
+
+debugLog = (output) => {
+  if (debug) {
+    console.log(YELLOW);
+    console.log(output);
+    console.log(NEUTRAL);
+  }
+}
+
+exists = (object, done) => {
+  assert.notEqual(object, null);
+  assert.notEqual(object, undefined);
+  done();
+}
 
 describe('Sunrise sunset', () => {
-  describe('/sunrise', () => {
-    it('should return -1 when the value is not present', () => {
 
-      place = { nimi: settings.sun_place_name, countrycode: settings.countrycode, lat: settings.sun_at_lat, lng: settings.sun_at_lon };
-
-      sunrise_sunset.getSunDataAtLocation(place, function (sun_data) {
-        let output = "<b>Dusk till dawn at Lahti, Finland:</b> \r\n";
-        output += sun_data;
-        const chatId = ctx.update.message.chat.id;
-        const extras = { parse_mode: 'Html' };
-        bot.telegram.sendMessage(chatId, output, extras).then(() => {
-          console.log("Message sent.");
-        })
+  describe('getSunDataAtLocation', () => {
+    it('should get parsed response from API', (done) => {
+      sunrise_sunset.getSunDataAtLocation(test_data.sunrise.place, (output) => {
+        exists(output, done);
       });
-
-      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
+
+});
+
+describe('VR trains', () => {
+
+  describe('haeAsemanTiedot', () => {
+    it('should get parsed response from API', (done) => {
+      train_parser.haeAsemanTiedot(test_data.train.stop1, (output) => {
+        debugLog(output);
+        exists(output, done);
+      });
+    });
+  });
+
+  describe('haeAsemanJunat', () => {
+    it('should get parsed response from API', (done) => {
+      train_parser.haeAsemanJunat(test_data.train.stop1, false, 10, (output) => {
+        debugLog(output);
+        exists(output, done);
+      });
+    });
+  });
+
+  describe('haeJunatReitille', () => {
+    it('should get parsed response from API', (done) => {
+      train_parser.haeJunatReitille(test_data.train.stop1, test_data.train.stop2, test_data.train.amount, false, (output) => {
+        debugLog(output);
+        exists(output, done);
+      });
+    });
+  });
+
+});
+
+describe('Weather', () => {
+
+  describe('getOpenWeatherData', () => {
+    it('should get parsed response from API', (done) => {
+      weather_parser.getOpenWeatherData(test_data.weather.place, test_data.weather.days, (output) => {
+        debugLog(output);
+        exists(output, done);
+      }, (err) => {
+        done(err);
+      });
+    });
+  });
+
+});
+
+describe('News', () => {
+
+  describe('getProvinces', () => {
+    it('should get parsed response from API', (done) => {
+      news_parser.getProvinces((output) => {
+        debugLog(output);
+        exists(output, done);
+      });
+    });
+  });
+
 });
