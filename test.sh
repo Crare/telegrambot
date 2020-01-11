a19c8993e797ba3c5b438f45303afef7a15642f2
crare <Crare@users.noreply.github.com>
2020-01-11 11:33:46 +0200

    update test

--- test.sh
+++ test.sh
@@ -8,47 +8,42 @@ sleep 1s
 
 echo ""
 echo "morning:"
-test="$(node bot_cronjob.js --message='TEST 1/9 morning:' -m)"
+test="$(node bot_cronjob.js --message='TEST 1/8 morning:' -m)"
 sleep 1s
 
 echo ""
 echo "evening:"
-test="$(node bot_cronjob.js --message='TEST 2/9 evening:' -e)"
+test="$(node bot_cronjob.js --message='TEST 2/8 evening:' -e)"
 sleep 1s
 
 echo ""
 echo "friday evening:"
-test="$(node bot_cronjob.js --message='TEST 3/9 friday evening:' -e -f)"
+test="$(node bot_cronjob.js --message='TEST 3/8 friday evening:' -e -f)"
 sleep 1s
 
 echo ""
 echo "weather:"
-test="$(node bot_cronjob.js --message='TEST 4/9 weather:' -W)"
+test="$(node bot_cronjob.js --message='TEST 4/8 weather:' -W)"
 sleep 1s
 
 echo ""
 echo "sunrise sunset:"
-test="$(node bot_cronjob.js --message='TEST 5/9 sunrise sunset:' -s)"
-sleep 1s
-
-echo ""
-echo "hours:"
-test="$(node bot_cronjob.js --message='TEST 6/9 hours:' -h)"
+test="$(node bot_cronjob.js --message='TEST 5/8 sunrise sunset:' -s)"
 sleep 1s
 
 echo ""
 echo "flagday:"
-test="$(node bot_cronjob.js --message='TEST 7/9 flagday:' -F)"
+test="$(node bot_cronjob.js --message='TEST 6/8 flagday:' -F)"
 sleep 1s
 
 echo ""
 echo "news:"
-test="$(node bot_cronjob.js --message='TEST 8/9 news:' -N)"
+test="$(node bot_cronjob.js --message='TEST 7/8 news:' -N)"
 sleep 1s
 
 echo ""
 echo "lunch week:"
-test="$(node bot_cronjob.js --message='TEST 9/9 lunch week:' --l_week)"
+test="$(node bot_cronjob.js --message='TEST 8/8 lunch week:' --l_week)"
 sleep 1s
 
 test="$(node bot_cronjob.js --message='running tests done')"
